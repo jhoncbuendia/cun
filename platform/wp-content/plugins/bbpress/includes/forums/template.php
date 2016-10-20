36a06c1c4178abfd30ed92ce9e041a6d73b31e97
iaderlorduy <iaderlor@gmail.com>
2016-10-20 10:59:35 -0500

    better comments

--- platform/wp-content/plugins/bbpress/includes/forums/template.php
+++ platform/wp-content/plugins/bbpress/includes/forums/template.php
@@ -750,7 +750,7 @@ function bbp_list_forums( $args = '' ) {
 
 	// Parse arguments against default values
 	$r = bbp_parse_args( $args, array(
-		'before'            => '<ul class="bbp-forums-list">oyemeeeeee!',
+		'before'            => '<ul class="bbp-forums-list">',
 		'after'             => '</ul>',
 		'link_before'       => '<li class="bbp-forum">',
 		'link_after'        => '</li>',
