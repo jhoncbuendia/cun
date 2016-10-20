bf9a0949b9805ada0d7389734143d8d648df7c0e
root <root@ip-172-31-29-62.us-west-2.compute.internal>
2016-10-20 15:23:24 +0000

    status

--- platform/wp-content/themes/Genesis/project.php
+++ platform/wp-content/themes/Genesis/project.php
@@ -70,105 +70,96 @@
         </div>
         <div class="col-md-3 inline-users">
             <div class="row">
-                <h3 class="title online">USUARIOS EN LÍNEA</h3>
-                <div class="row the-users">
+                <h3 class="title online">USUARIOS EN LÍNEA</h3>	
+		  
+<?php if ( bp_has_members( bp_ajax_querystring( 'members' ) ) ) : ?> 
+ 
+  <?php do_action( 'bp_before_directory_members_list' ); ?> 
+ 
+  <div id="members-list" class="item-list" role="main">
+ 
+  <?php while ( bp_members() ) : bp_the_member(); ?>
+
+  <div class="row the-users">
                     <div class="col-md-2 image-profile">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
+                       <a href="<?php bp_member_permalink(); ?>"><?php bp_member_avatar(); ?></a>
                     </div>
                     <div class="col-md-8 name-user">
-                        SANTIAGO TORRES
+                         <a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a>
                     </div>
-                </div>
-                <div class="row the-users">
-                    <div class="col-md-2 image-profile">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-8 name-user">
-                        SANTIAGO TORRES
-                    </div>
-                </div>
-                <div class="row the-users">
-                    <div class="col-md-2 image-profile">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-8 name-user">
-                        SANTIAGO TORRES
-                    </div>
-                </div>
-                <div class="row the-users">
-                    <div class="col-md-2 image-profile">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-8 name-user">
-                        SANTIAGO TORRES
-                    </div>
-                </div>
-                <div class="row the-users">
-                    <div class="col-md-2 image-profile">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-8 name-user">
-                        SANTIAGO TORRES
-                    </div>
-                </div>
+  </div>
+    
+ 
+ <?php endwhile; ?>
+ 
+ </div>
+ 
+ <?php do_action( 'bp_after_directory_members_list' ); ?>
+ 
+ <?php bp_member_hidden_fields(); ?>
+ 
+ <div id="pag-bottom" class="pagination">
+ 
+    <div class="pag-count" id="member-dir-count-bottom">
+ 
+       <?php bp_members_pagination_count(); ?>
+ 
+    </div>
+ 
+    <div class="pagination-links" id="member-dir-pag-bottom">
+ 
+      <?php bp_members_pagination_links(); ?>
+ 
+    </div>
+ 
+  </div>
+ 
+<?php else: ?>
+ 
+   <div id="message" class="info">
+      <p><?php _e( "Sorry, no members were found.", 'buddypress' ); ?></p>
+   </div>
+ 
+<?php endif; ?>
+
             </div>
             <div class="row">
                 <h3 class="title comments">COMENTARIOS RECIENTES</h3>
-                <div class="row last-comments">
+
+<?php
+ if( $comments = $wpdb->get_results(
+   "SELECT comment_author, comment_author_url,
+    comment_ID, comment_post_ID
+   FROM $wpdb->comments
+   WHERE comment_approved = '1'
+   ORDER BY comment_date_gmt DESC LIMIT 15") ) :
+
+    global $comment;
+    foreach ($comments as $comment) { ?>
+<div class="row last-comments">
                     <div class="col-md-3 profile-image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
+                        <a href="<?php bp_member_permalink(); ?>"><?php echo get_avatar($comment, 32 ); ?></a>
                     </div>
                     <div class="col-md-9">
-                        <h6>FRANK TORRES</h6>
+                        <h6><?php echo get_comment_author_link() ?></h6>
                         <content>
-                            <p>Donec semper tortor finibus, fermentum odio sit amet, bibendum nunc.</p>    
+                            <p>
+                              <?php 
+ 
+echo '<a href="' . get_permalink($comment->comment_ID)
+. '#comment-' . $comment->comment_ID . '">' 
+. get_the_title($comment->comment_post_ID) 
+. '</a>'; ?>
+                            </p>    
                         </content> 
                     </div>
                 </div>
-                <div class="row last-comments">
-                    <div class="col-md-3 profile-image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-9">
-                        <h6>FRANK TORRES</h6>
-                        <content>
-                            <p>Donec semper tortor finibus, fermentum odio sit amet, bibendum nunc.</p>    
-                        </content> 
-                    </div>
-                </div>
-                <div class="row last-comments">
-                    <div class="col-md-3 profile-image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-9">
-                        <h6>FRANK TORRES</h6>
-                        <content>
-                            <p>Donec semper tortor finibus, fermentum odio sit amet, bibendum nunc.</p>    
-                        </content> 
-                    </div>
-                </div>
-                <div class="row last-comments">
-                    <div class="col-md-3 profile-image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-9">
-                        <h6>FRANK TORRES</h6>
-                        <content>
-                            <p>Donec semper tortor finibus, fermentum odio sit amet, bibendum nunc.</p>    
-                        </content> 
-                    </div>
-                </div>
-                <div class="row last-comments">
-                    <div class="col-md-3 profile-image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col-md-9">
-                        <h6>FRANK TORRES</h6>
-                        <content>
-                            <p>Donec semper tortor finibus, fermentum odio sit amet, bibendum nunc.</p>    
-                        </content> 
-                    </div>
-                </div>
+
+ <?php   }
+    endif; ?>
+
+                
+                
             </div>
         </div>
     </section>
