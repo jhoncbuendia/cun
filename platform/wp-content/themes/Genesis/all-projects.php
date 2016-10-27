585b8a2a1cc6ec3b854ea5fa9ba35bff7959804c
Frank Torres <rasfuranku@gmail.com>
2016-10-27 11:34:07 -0500

    added project template and projects template

---
+++ platform/wp-content/themes/Genesis/all-projects.php
@@ -0,0 +1,67 @@
+<?php
+/* Template Name: Todos los proyectos */
+    get_header();
+?>
+<section class="container navigation-options">
+    <div class="row">
+        <div class="col-md-6"><?php dynamic_sidebar( 'Breadcrumb' ); ?></div>
+        <div class="col-md-6">
+            <div class="dropdown change-line">
+                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    CAMBIAR RED <img src="<?php echo get_template_directory_uri(); ?>/images/little-cow.png" alt="">
+                    <span class="caret"></span>
+                </button>
+                <?php wp_nav_menu( array( 'theme_location' => 'menu-knowlege-all', 'menu_class' =>  'dropdown-menu', 'container' => 'ul') ); ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php while ( have_posts() ) : the_post();
+    // Include the single post content template.
+    get_template_part( 'template-parts/content', 'single' );
+?>
+<section class="container proyects">
+    <div class="col-md-9 search-proyects">
+        <h3 class="title">PROYECTOS</h3>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                <?php
+                    $args = array( 'posts_per_page' => 4, 'offset'=> 0, 'category' => 'Ciencias Socioculturales' );
+
+                    $myposts = get_posts( $args );
+                    foreach ( $myposts as $post ) : setup_postdata( $post );
+                ?>
+                    <div class="col-md-6">
+                        <div class="col-md-4 image-article">
+                            <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail(array(150, 153));
+                                }
+                            ?>
+                        </div>
+                        <div class="col-md-8 content-article">
+                            <h5 class="title-article"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                            <content>
+                                <?php echo excerpt(20); ?>
+                            </content>
+                            <a href="<?php the_permalink() ?>" class="know-more">CONOCER MÁS</a>
+                        </div>
+                    </div>
+                <?php endforeach;
+                    wp_reset_postdata();
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 inline-users">
+        <?php dynamic_sidebar( 'online_users' ); ?>
+        <div class="row">
+            <?php dynamic_sidebar( 'last_comments' ); ?>
+        </div>
+    </div>
+</section>
+<?php
+    get_footer();
+?>
---
+++ platform/wp-content/themes/Genesis/the-project.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ * @subpackage Genesis
+ * @since Genesis 1.0
+ */
+    get_header();
+?>
+    <section class="container project">
+        <div class="col-md-9 content-proyect">
+            <div class="row first-part">
+                <div class="row divided-content">
+                    <div class="col-md-6 left-content">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h5>Título de la investigación</h5>
+                                <div class="item-project title-project">
+                                    EL ARTE:  UNA FORMA DE APRENDER SOBRE SEXUALIDAD
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h5>Red del conocimiento:</h5>
+                                <div class="item-project">
+                                    Ciencias Exactas Socioculturales
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h5>Municipio:</h5>
+                                <div class="item-project">
+                                    Tierralta
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 right-content">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="logo-project"></div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 institution-project">
+                                <h5>Institución educativa:</h5>
+                                <div class="item-project">
+                                    Ciencias Exactas Socioculturales
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="problem-project">
+                    <h3 class="title-sections">1. Planteamiento del problema</h3>
+                    <article class="item-project">
+                        <h4>Pregunta de Investigación</h4>
+
+                        ¿Será posible prevenir los embarazos no deseados, las enfermedades de transmisión sexual y la violencia de género en los estudiantes de la Institución educativa la Inmaculada de Tierralta a través de las artes escénicas y otras actividades metodológicas lideradas por el grupo de teatro Caragabí? 
+
+                        “La Institución Educativa La Inmaculada donde se desarrolla el proyecto,  maneja una población estudiantil con un nivel socioeconómico medio- bajo que está expuesta a un contexto social vulnerado por la violencia común y los grupos al margen de la ley. Esta situación lleva a los jóvenes a una alta fragilidad  frente a los flagelos como la droga, el alcoholismo, la prostitución y comportamientos delincuenciales, y en ocasiones a violencia intrafamiliar. 
+
+                        Por ello, el presente trabajo conllevó a que el grupo de docentes y estudiantes de la I. E. la Inmaculada, se reunieran a crear estrategias de prevención y formación para contribuir en la solución de las problemáticas planteadas.  De esta manera se crea  el grupo de teatro Caragabí, (nombre en honor a los Embera) conformados por 15 estudiantes de la Institución del grado 9°  y  edades  que oscilan entre los 14 y 16 años. Dentro de este grupo hacen parte integrantes de la población tanto  Embera como afro, favoreciendo con ello la inclusividad de estas poblaciones”. 
+                    </article>
+                </div>
+            </div>
+            <div class="row second-part">
+                <div class="problem-project">
+                    <h3 class="title-sections">2. Objetivos</h3>
+                    <article class="item-project">
+                        <h4>Objetivo General: </h4>
+                        Educar en temas sobre sexualidad, salud y construcción de ciudadanía  a los estudiantes de la Institución educativa la Inmaculada para prevenir  los embarazos no deseados, las enfermedades de transmisión sexual y la violencia de género a través de las artes escénicas y otras actividades artísticas lideradas por el Grupo de Teatro Caragabí.
+                        <h4>Objetivo Específicos: </h4>
+                        Identificar las causas que promueven los embarazos no deseados, las enfermedades de transmisión sexual y la violencia de género en la población estudiantil de la Institución educativa la Inmaculada. <br>  
+                        Realizar representaciones teatrales para orientar a los jóvenes en temas sobre sexualidad, salud y construcción de ciudadanía. <br>
+                        Trabajar conjuntamente con los servicios amigables para apoyar el trabajo pedagógico que se desarrolla con los estudiantes en materia de sexualidad. <br>
+                        Promover la conciencia del ejercicio de los derechos sexuales y reproductivos con responsabilidad y ética ciudadana a través de charlas realizadas por el grupo de Teatro Caragabí y el personal especializado ofrecido por los servicios amigables. <br> 
+                        Realizar actividades comunitarias que integren al grupo de teatro Caragabí en el ejercicio de la sana convivencia a través del cuerpo. <br>
+                    </article>
+                </div>
+                <div class="problem-project">
+                    <h3 class="title-sections">3. Metodología</h3>
+                    <article class="item-project">
+                        “La dimensión de esta investigación es medible y se ha realizado de manera mixta: tanto cualitativa como cuantitativamente”.
+                    </article>
+                </div>
+                <div class="problem-project">
+                    <h3 class="title-sections">4. Resultados</h3>
+                    <article class="item-project">
+                        “Formación pedagógica de los estudiantes en los conocimientos   de los derechos sexuales y reproductivos.
+                        Disminución en los embarazos no deseados, las enfermedades de transmisión sexual y la violencia de género en los estudiantes de la Institución educativa la Inmaculada”
+                    </article>
+                </div>
+            </div>
+            <div class="row third-part">
+                <div class="problem-project">
+                    <h3 class="title-sections">2. Bibliografía</h3>
+                    <article class="item-project">
+                        Prentki T.  y  Lacey C. (2014) Uso del teatro en el desarrollo. Revista paso a paso. N°18. recuperado de http://tilz.tearfund.org/es-es/resources/publications/footsteps/footsteps_51-60/footsteps_58/using_theatre_in_development/
+                        <br><br>
+                        Marina, J. (2013). El rompecabezas de la sexualidad, Anagrama. Barcelona. P. 9. 
+                         <br><br>
+                        González Barra, A. (2011). La sexualidad en el siglo XXI a través del arte. Recuperado de https://ensayandolibertad.wordpress.com/2011/05/16/la-sexualidad-en-el-siglo-xxi-a-traves-del-arte/ 
+                         <br><br>
+                        Instituto Colombiano de Bienestar Familiar (2008) . Módulos pedagógicos sobre derechos sexuales y reproductivos. Bogotá. Recuperado de http://www.icbf.gov.co/portal/page/portal/PortalICBF/RecursosMultimedia/Publicaciones/Editoriales1/Drechossexualesyreproductivos.pdf 
+                         <br><br>
+                        Ministerio de Salud y la Protección Social (2014) . Piloto de prevención de embarazo en adolescentes. Recuperado de http://www.colombiaaprende.edu.co/html/productos/1685/w3-article-345755.html
+                    </article>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 inline-users">
+            <div class="row">
+                <h3 class="title online gruop-title">GRUPO DE TEATRO CARAGABÍ</h3>
+                <div class="row the-users">
+                    <div class="col-md-2 image-profile">
+                        <img src="images/profile.jpg" alt="">
+                    </div>
+                    <div class="col-md-8 name-user">
+                        <h6 class="rol-user">Docente</h6>
+                        <h6 class="user-project">FRANK TORRES</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php get_footer(); ?>
