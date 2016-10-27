cc33fc4696a477e47b98b92f7f096bc60c4bf40c
Frank Torres <rasfuranku@gmail.com>
2016-10-27 11:45:35 -0500

    bootstrap slider

--- platform/wp-content/themes/Genesis/index.php
+++ platform/wp-content/themes/Genesis/index.php
@@ -16,9 +16,41 @@
     get_header();
 ?>
     <section class="container">
-        <?php 
-            echo do_shortcode("[metaslider id=133]"); 
-        ?>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/Banner1.jpg" alt="...">
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/Banner2.jpg" alt="...">
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/Banner3.jpg" alt="...">
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/Banner4.jpg" alt="...">
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     </section>
     <section class="container" id="knowledges-lines">
         <div class="row">
